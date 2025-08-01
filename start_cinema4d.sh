66404e02fe8fdbca389b955fe762d6dbd05b0b6d
David Leong <116610336+leongdl@users.noreply.github.com>
2025-08-01 02:28:21 +0000

    Trying more versions of c4d

--- start_cinema4d.sh
+++ start_cinema4d.sh
@@ -3,8 +3,8 @@
 # Start Cinema4D Docker container with proper environment setup
 sudo docker run -it --rm --network host --ulimit stack=52428800 cinema4d:latest bash -c "
     echo 'Setting up Cinema4D environment...'
-    source /opt/maxon/cinema4dr2025.301/bin/setup_c4d_env
-    cd /opt/maxon/cinema4dr2025.301/bin
+    source /opt/maxon/cinema4dr2025.002/bin/setup_c4d_env
+    cd /opt/maxon/cinema4dr2025.002/bin
     echo \"Cinema4D environment ready. Current directory: \$(pwd)\"
     exec bash
 "
